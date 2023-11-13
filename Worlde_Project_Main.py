8dc3be2e6ce7cfd9a489ee1e4f2f245f1325e8b4
higgityhayden <149332284+higgityhayden@users.noreply.github.com>
2023-11-12 22:03:47 -0500

    Update Worlde_Project_Main.py
    These changes are to reflect recent updates to the file.

--- Worlde_Project_Main.py
+++ Worlde_Project_Main.py
@@ -1,31 +1,63 @@
-# Creating list of characters from solution
-solution = "angle"
-solution_list = []
-for i in solution:
-  solution_list.append(i)
+from english_words import get_english_words_set
+import datetime
 
-# Logic to ensure that the selected choice is only 5 letters
-choice_one = (input("Wordle selection: "))
-while int(len(choice_one)) != 5:
-  choice_one = (input("[Only 5 letters long pls] Wordle selection: "))
+# Define a list of words to choose from
+web2lowerset = get_english_words_set(['web2'], lower=True)
 
-# Creating list of characters from choice
-choice_list = []
-for i in choice_one:
-  choice_list.append(i)
+# Filter 5-letter words from the word list
+five_letter_words = [word for word in web2lowerset if len(word) == 5]
 
-def matcher_func(char_list, sol_list):
-  c_list = char_list
-  s_list = sol_list
-  for char in range(len(c_list)):
-    if c_list[char] == s_list[char]:
-      del c_list[char]
-      del s_list[char]
-      print(f"Position {char + 1}: One of the characters is in the right position: {c_list[char]}")
-    elif c_list[char] in s_list:
-      print(f"Position {char + 1}: One of the characters is in position {c_list[char]}")
-    else:
-      print(f"Position {char + 1}: No character match")
+# Get the current day of the year (1 to 365)
+day_of_year = datetime.date.today().timetuple().tm_yday
+
+# Calculate the index for the word of the day
+word_index = day_of_year % len(five_letter_words)
+
+# Get the word of the day
+word_of_the_day = five_letter_words[word_index]
 
 
-matcher_func(choice_list, solution_list)
+# Function to evaluate the user's input
+def evaluate_guess(secret_word, guess):
+    result = []
+    for i in range(len(secret_word)):
+        if guess[i] == secret_word[i]:
+            result.append("Match")
+        elif guess[i] in secret_word:
+            result.append("Close Match")
+        else:
+            result.append("No Match")
+    return result
+
+
+#Function to execute program
+def play_wordle():
+    secret_word = word_of_the_day
+    attempts = 0
+
+    print("Welcome to Wordle!")
+    print("You have 6 attempts to guess a five-letter word.")
+
+    while attempts < 6:
+        guess = input(f"Attempt {attempts + 1}: ").lower()
+        while guess not in five_letter_words:
+            print("Please enter a five-letter word")
+            guess = input(f"Attempt {attempts + 1}: ").lower()
+
+        if guess == secret_word:
+            print(f"Congratulations! You've guessed the word: {secret_word}")
+            break
+
+        feedback = evaluate_guess(secret_word, guess)
+        print("Feedback:")
+        for letter, color in zip(guess, feedback):
+            print(f"{letter}: {color}")
+
+        attempts += 1
+
+    if attempts == 6:
+        print(f"Sorry, you've used all your attempts. The word was: {secret_word}")
+
+
+if __name__ == "__main__":
+    play_wordle()
